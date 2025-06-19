2b0c74e16c5513e5d427fb64d1ed665033aca5d6
David Zapata <davidalejandrozapata2@gmail.com>
2025-06-19 18:37:50 -0500

    first commit

---
+++ functions.js
@@ -0,0 +1,19 @@
+function añadirMascota(){
+    //Funcion para añadir mascotas
+}
+
+function buscarMascota(){
+    //Funcion para buscar mascotas por nombre
+}
+
+function filtrarVacunador(){
+    //funcion para filtrar vacunados con funciones flecha
+}
+
+function calcularPromedioEdad(){
+    //calcular promedio de edades
+}
+
+function mostrarOrdenEdad(){
+    //Organizar mascotas por orden de edad
+}
